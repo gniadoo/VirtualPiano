b45dde5d2e7bb43d555fa202a1d3eb34f7f33899
gniadoo <gniadek.jakub1@gmail.com>
2021-08-26 17:26:22 +0200

    Adding sound effect

--- Virtual Piano/task/src/main.js
+++ Virtual Piano/task/src/main.js
@@ -2,7 +2,8 @@ let valid_keys = ['KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ'];
 
 document.addEventListener("keypress", function(e) {
     if(valid_keys.includes(e.code)) {
-        console.log(`The \'${e.key}\' is pressed`);
+        let audio = new Audio("../sounds/" + String(e.key.toUpperCase()) + ".mp3");
+        audio.play();
     } else {
         console.log("Warning");
     }
